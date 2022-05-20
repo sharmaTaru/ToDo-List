19fd579eebc73672b32f304373414729ae40c61c
Taru <tarusharma9090@gmail.com>
2022-05-20 19:47:00 +0530

    setup controllers and routers and modified name of index.js to ToDo.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+
+app.use('/', require('./Routes'));
+
 app.set('view engine','ejs');
 app.set('views','./Views');
-
-
 
 
 app.listen(port, function(err){
