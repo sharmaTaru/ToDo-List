057078a27b682eb4dd068688246f4eb146daae34
Taru <tarusharma9090@gmail.com>
2022-05-20 13:48:20 +0530

    setup the server and view engine

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.set('view engine','ejs');
+app.set('views','./Views');
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error connecting to the server' , err);
+    }
+    console.log(`Successfully connected to the server on port: ${port}` );
+})
