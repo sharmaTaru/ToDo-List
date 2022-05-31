4b057c4b1ad1f476277baeb6739927b8ba16b990
Taru <tarusharma9090@gmail.com>
2022-05-31 21:34:05 +0530

    cleaned the code of deleteTask_controller

--- Controllers/deleteTask_controller.js
+++ Controllers/deleteTask_controller.js
@@ -1,10 +1,7 @@
 require("../Config/mongoose");
 const Task = require("../Modals/tasks");
-
-// let deleteButton = .getElementById('deleteButton')
 module.exports.deleteTask = function(req,res){
     let id = req.query.id;
-    console.log(id,req.xhr);
     Task.findByIdAndDelete(id,function(err,taskToDelete){
         if(err){
             console.log(`Error in deleting task ${err}`);
