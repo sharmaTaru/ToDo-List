9ae03f4e01480d02e889093f45aa8ea0931d1538
Taru <tarusharma9090@gmail.com>
2022-05-31 21:32:39 +0530

    Made a functionality in the webpage to delete multiple files at once

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -1,3 +1,11 @@
-
-
-   
+var a=[];
+function checks(id){
+    a.push(id);
+}
+async function mulDel(){
+    console.log(a);
+    for(let i of a){
+       await fetch(`/deleteTask/?id=${i}`);
+    }
+window.location.reload();
+}
--- Controllers/deleteTask_controller.js
+++ Controllers/deleteTask_controller.js
@@ -1,7 +1,10 @@
+require("../Config/mongoose");
 const Task = require("../Modals/tasks");
+
 // let deleteButton = .getElementById('deleteButton')
 module.exports.deleteTask = function(req,res){
     let id = req.query.id;
+    console.log(id,req.xhr);
     Task.findByIdAndDelete(id,function(err,taskToDelete){
         if(err){
             console.log(`Error in deleting task ${err}`);
